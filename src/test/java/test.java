ef415d2e7cc02c4ca21ae2dc88017c9a714e03ec
satya <satyamjha06@gmail.com>
2025-05-06 11:07:59 -0400

    second commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,7 +2,8 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("this is project Version1");
+        System.out.println("this is project Version2");
+        System.out.println("this project is for practice");
 
     }
 }
