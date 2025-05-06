0308c6b154da90045d7e9548fd1639e40f92b356
satya <satyamjha06@gmail.com>
2025-05-06 11:11:20 -0400

    third commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,8 +1,8 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("this is project Version2");
+
+        System.out.println("this is project Version3");
         System.out.println("this project is for practice");
 
     }
