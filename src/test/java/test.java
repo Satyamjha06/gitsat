783f91517effeed3f82b4aa7a4ef8173f515da39
satya <satyamjha06@gmail.com>
2025-05-12 16:48:07 -0400

    fourth commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,8 +2,8 @@ public class test {
 
     public static void main(String[] args) {
 
-        System.out.println("this is project Version3");
-        System.out.println("this project is for practice");
+        System.out.println("this is project Version4");
+
 
     }
 }
